69c2e0dc404be3c67e1e7ec5820f69c6af670753
MajorTooooom <2414566866@qq.com>
2020-03-21 17:57:36 +0800

    Cluster environment configuration
    完成了注册中心的集群配置，大致思路是模拟了三个注册中心互相关联，且一个微服务发布到所有注册中心的测试。

---
+++ springcloud-eureka-7002/src/main/java/com/chengfeng/springcloud/EurekaServer_7002.java
@@ -0,0 +1,13 @@
+package com.chengfeng.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //表示是一个服务端的启动类,它可以接受别人注册进来
+public class EurekaServer_7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7002.class, args);
+    }
+}
