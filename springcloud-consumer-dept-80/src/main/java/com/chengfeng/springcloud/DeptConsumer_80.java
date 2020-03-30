b9c80c6038a28f0fdd436d9e7987f6d074ca4339
MajorTooooom <2414566866@qq.com>
2020-03-30 21:36:22 +0800

    Load balancing with ribbon
    用ribbon实现负载均衡。测试方法：三个服务提供者（8001、8002、8003）集成ribbon，想注册中心里面注册自己，当服务消费者去消费的时候发现ribbon默认轮询规则。

--- springcloud-consumer-dept-80/src/main/java/com/chengfeng/springcloud/DeptConsumer_80.java
+++ springcloud-consumer-dept-80/src/main/java/com/chengfeng/springcloud/DeptConsumer_80.java
@@ -2,8 +2,10 @@ package com.chengfeng.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptConsumer_80 {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer_80.class, args);
--- springcloud-consumer-dept-80/src/main/java/com/chengfeng/springcloud/controller/DeptConsumerController.java
+++ springcloud-consumer-dept-80/src/main/java/com/chengfeng/springcloud/controller/DeptConsumerController.java
@@ -2,7 +2,6 @@ package com.chengfeng.springcloud.controller;
 
 import com.chengfeng.springcloud.pojo.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +16,11 @@ public class DeptConsumerController {
     //提供多种便捷访问远程http服务的方法，简单的restful服务模块
     @Autowired
     private RestTemplate restTemplate;
-    public static final String REST_URL_PREFIX = "http://localhost:8001/";
+
+    //未实现负载均衡之前url地址是常量，使用 public static final String REST_URL_PREFIX = "http://localhost:8001/";
+//    public static final String REST_URL_PREFIX = "http://localhost:8001/";
+//实现负载均衡之后url地址应该是变量：此时通过服务名来访问，因为当一个服务就算发布到多个注册中心的时候，其application名称是不变的
+    public static final String REST_URL_PREFIX = "http://SPRINGCLOUD-PROVIDER-DEPT";
 
     @RequestMapping("/consumer/dept/add")
     public boolean add(Dept dept) {
