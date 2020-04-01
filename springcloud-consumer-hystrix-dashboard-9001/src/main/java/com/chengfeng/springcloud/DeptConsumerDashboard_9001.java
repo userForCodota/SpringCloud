97b7e3ade9e064da715ae5b47a46221df81a0f9d
MajorTooooom <2414566866@qq.com>
2020-04-01 16:14:44 +0800

    Using hystrix to monitor dashboard flow.
    使用hystrix实现Dashboard流监控。
    1）新建module：springcloud-consumer-hystrix-dashboard-9001；
    2）引入依赖，编写servlet（bean）；
    3）开启功能（开关）；
    4）（被监测的）服务提供者（springcloud-provider-dept-hystrix-8001）添加hystrix依赖（之前已经添加了actuator依赖）；
    5）打开测试页面，发起一些成功/失败的请求进行测试。

--- springcloud-consumer-hystrix-dashboard-9001/src/main/java/com/chengfeng/springcloud/DeptConsumerDashboard_9001.java
+++ springcloud-consumer-hystrix-dashboard-9001/src/main/java/com/chengfeng/springcloud/DeptConsumerDashboard_9001.java
@@ -1,22 +1,17 @@
 package com.chengfeng.springcloud;
 
-
 import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 
-//将这个类作为主启动类
 @SpringBootApplication
-@EnableEurekaClient //注解EnableEurekaClient表示此app在服务启动后自动注册到eureka中
-@EnableDiscoveryClient //开启服务发现（扩展一下）
-public class DeptProvider_8001 {
+@EnableHystrixDashboard //开启监控页面，同时在“服务提供者”中也要添加相对应的依赖，其才能被监测到
+public class DeptConsumerDashboard_9001 {
     public static void main(String[] args) {
-        SpringApplication.run(DeptProvider_8001.class, args);
+        SpringApplication.run(DeptConsumerDashboard_9001.class, args);
     }
-
 
 }
--- springcloud-provider-dept-hystrix-8001/src/main/java/com/chengfeng/springcloud/DeptProviderHystrix_8001.java
+++ springcloud-provider-dept-hystrix-8001/src/main/java/com/chengfeng/springcloud/DeptProviderHystrix_8001.java
@@ -1,11 +1,14 @@
 package com.chengfeng.springcloud;
 
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 //将这个类作为主启动类
 @SpringBootApplication
@@ -15,5 +18,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class DeptProviderHystrix_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProviderHystrix_8001.class, args);
+    }
+
+    //增加一个servlet
+    @Bean
+    public ServletRegistrationBean a() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        return registrationBean;
     }
 }
